f8a0313548ce8e73bbd3e850d864638b458ed418
Jan-Torge Schindler <schindler@mpia.de>
2021-04-28 09:13:03 +0200

    added plot script

---
+++ scripts/pypeit_help_onedplot.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+
+import argparse
+import plot
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="""
+            Quick plotting routine for 1D spectra using pypeit-helpers. 
+            """,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('filename', type=str,
+                        help='Filename of spectrum to plot')
+
+    parser.add_argument('-s', '--smooth', required=False, type=float,
+                        help='Number of pixels for a simple boxcar smoothing '
+                             'of the spectrum.',
+                        default=None)
+
+    parser.add_argument('-ymax', '--ymax', required=False, type=float,
+                        help='Setting the maximum value for the y-axis.',
+                        default=None)
+
+    parser.add_argument('-ymin', '--ymin', required=False, type=float,
+                        help='Setting the maximum value for the y-axis.',
+                        default=None)
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+
+    args = parse_arguments()
+
+    plot.plot_pypeit_spectrum(args.filename,
+                              smooth=args.smooth,
+                              ymin=args.ymin,
+                              ymax=args.ymax)
+
